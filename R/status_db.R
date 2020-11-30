1e145d8318bd62c7705dfec8d6618525ed4f7a11
Vince Carey <stvjc@channing.harvard.edu>
2020-11-30 16:13:25 -0500

    preparing database port

---
+++ R/status_db.R
@@ -0,0 +1,12 @@
+#' initialize a status database
+#' @param sqlite_filename character(1) typically new path
+#' @param named_df_list a list of data.frame instances
+#' @param exists_ok logical(1) if TRUE will work with existing db
+status_db_init = function(sqlite_filename, named_df_list, exists_ok=FALSE) {
+  if (!exists_ok & file.exists(sqlite_filename)) stop(sqlite_filename, " exists and overwrite = FALSE")
+  con = dbConnect(RSQLite::SQLite(), sqlite_filename)
+  nms = names(named_df_list)
+  for (i in nms)
+    dbWriteTable(con, i, named_df_list[[i]])
+  invisible(nms)
+}
