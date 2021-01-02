8119cf8fc9121eab4d7b708b6e4f917cf5580b82
Vince Carey <stvjc@channing.harvard.edu>
2021-01-01 20:24:31 -0500

    allow fulfill_deps to use parallelism

--- R/fulfill_deps.R
+++ R/fulfill_deps.R
@@ -3,6 +3,7 @@
 #' @param depdf a data.frame like the output of `BiocPkgTools::buildPkgDependencyDataFrame()`
 #' @param exclude character() of uninstalled packages to be ignored -- BiocManager::install is slow
 #' to deal with an unavailable package
+#' @param \dots passed to BiocManager::install
 #' @return NULL
 #' @note Function is used only for side-effect of installation.  `exclude` defaults to
 #' 'KEGG.db' because in Bioc 3.13 that is not available.  It is understood that code
@@ -10,7 +11,7 @@
 #' concerns software ecosystem management and must be able to programmatically
 #' define the ecosystem for evaluation.
 #' @export
-fulfill_deps = function(pkg, depdf = NULL, exclude="KEGG.db") {
+fulfill_deps = function(pkg, depdf = NULL, exclude="KEGG.db", ...) {
  oopt = options(no.readonly=TRUE)
  on.exit(options(oopt))
  options(timeout=300)
@@ -19,6 +20,6 @@ fulfill_deps = function(pkg, depdf = NULL, exclude="KEGG.db") {
  dat = depdf |> dplyr::filter(Package == pkg)
  must_get = setdiff(dat$dependency, c("R", ii, exclude))
  if (length(must_get)>0) message(paste("running BiocManager::install on", must_get))
- if (length(must_get)>0) BiocManager::install(must_get)
+ if (length(must_get)>0) BiocManager::install(must_get, ...)
  return(NULL)
 }
